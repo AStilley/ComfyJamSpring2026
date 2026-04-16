ec6d6426da9ec5a06f554633a33d476523b30ff2
Awesdmon <Gabrielpierre28@gmail.com>
2026-04-15 20:07:03 -0400

    dust
    fixed

--- game-jam-spring-2026/Dusting Mini Game/dust_minigame.gd
+++ game-jam-spring-2026/Dusting Mini Game/dust_minigame.gd
@@ -1,7 +1,16 @@
 extends Node2D
-@onready var sprite = $Area2D
+
+@onready var sprite = $Sprite2D
 
 func Startgame(object):
 	sprite.texture = object
 	
+	var target_size = Vector2(300, 300) # desired size in pixels
+	var tex_size = sprite.texture.get_size()
+	
+	sprite.scale = target_size / tex_size
+	
+	sprite.setup_dust()
+func _ready() -> void:
+	Startgame(load("res://icon.svg"))
 	
